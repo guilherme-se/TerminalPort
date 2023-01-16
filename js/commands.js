16b793eecae26196d8c44315d69bd086fd679cee
guilherme-se <guilhermeroroni21@gmail.com>
2023-01-15 21:11:34 -0300

    first commit

---
+++ js/commands.js
@@ -0,0 +1,85 @@
+var password = "hired";
+var linkedin = "https://www.linkedin.com/in/guilherme-santos-espindula/";
+var github = "https://github.com/guilherme-se";
+var email = 'mailto:guilhermesantosespind@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Guilherme santos!",
+  "I'm a software developer and content creator, who builds engaging websites like this one",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of Who am I? we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/guilhermesantos' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/guilherme-se' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Guilherme Santos??',
+  '<span class="command">whoami</span>         Who are You?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">clear</span>          Clear terminal',
+  "<br>",
+];
+
+banner = [
+  '<span class="index"> (c) GSE Corporation is not a Corporation. All rights reserved.</span>',
+  "                               ",
+  "                 .@@@@@@@@:.  ",
+  "                @@@@@@@@.@@@@@.  ",
+  "              .@@@@@@@@@@@@@@@@.  ",
+  "              @@@@@@@@@@@@@@@@@@  ",
+  "              @@' _`@@'_  `@@@@@  ",
+  "              @@ @@ @@ @@  @@@@@  ",
+  "              @@_@@_::_@@_:@@@@@  ",
+  "              @@:::,::,:::::@@@@                        ________  ________    __  ____________  __  _________   _____ ___    _   ____________  _____",
+  "              @@`:::::::::'`@@@@                       / ____/ / / /  _/ /   / / / / ____/ __ \\/  |/  / ____/  / ___//   |  / | / /_  __/ __ \\/ ___/",
+  "             .@@  `::::'    @:@@.                     / / __/ / / // // /   / /_/ / __/ / /_/ / /|_/ / __/     \\__ \\/ /| | /  |/ / / / / / / /\\__ \\",
+  "            @@@@            `@:@@@.                  / /_/ / /_/ // // /___/ __  / /___/ _, _/ /  / / /___    ___/ / ___ |/ /|  / / / / /_/ /___/ /",
+  "          .@@@@'             `@@@@@@.                \\____/\\____/___/_____/_/ /_/_____/_/ |_/_/  /_/_____/   /____/_/  |_/_/ |_/ /_/  \\____//____/",
+  "         .@@@@:              '@@@@@@@:.  ",
+  "        .@@@@.                :`@@:@@@@@             ____  ___   ________ __ _______   ______     ____  _______    __________    ____  ____  __________   ",
+  "       .@@@@                   .@@@:@@@@.           / __ )/   | / ____/ //_// ____/ | / / __ \\   / __ \\/ ____/ |  / / ____/ /   / __ \\/ __ \\/ ____/ __ \\ ",
+  "      @@@:@                     @@@:@@@@@          / __  / /| |/ /   / ,<  / __/ /  |/ / / / /  / / / / __/  | | / / __/ / /   / / / / /_/ / __/ / /_/ /",
+  "    .@@@:@@                     @@@:@@@@@:        / /_/ / ___ / /___/ /| |/ /___/ /|  / /_/ /  / /_/ / /___  | |/ / /___/ /___/ /_/ / ____/ /___/ _, _/ ",
+  "    @@@@@@@.                    @@:@@@@@@        /_____/_/  |_\\____/_/ |_/_____/_/ |_/_____/  /_____/_____/  |___/_____/_____/\\____/_/   /_____/_/ |_|  ",
+  "    `.::.@@@.                  .@@@@@@@@  ",
+  "   .::::::.@@@.               :::`@@@@'.:.  ",
+  "  ::::::::::.@@@             .::::::::::::  ",
+  "  ::::::::::::.@           .:@::::::::::::.  ",
+  " .::::::::::::::.        .:@@@:::::::::::::  ",
+  " :::::::::::::::@@:.__..:@@@@@:::::::::::'  ",
+  "  `'.:::::::::::@@@@@@@@@@@.@@:::::::::'  ",
+  "     `':::_:' -- '' -'-' `':_::::'`  ",
+  "                                             ",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
